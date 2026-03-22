a43e64657339d6810e760d652e349b8717ddd358
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-03-22 22:58:31 +0000

    Separate concerns and create basic input logic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
+const form = document.querySelector("form");
+const inputField = document.querySelector("input");
+
+form.addEventListener("submit", (event) => {
+  const userInput = inputField.value;
+  event.preventDefault();
+  weatherApp(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput}/?key=BTQL28EQ6TJJ8CBVW3QNHP987`,
+  );
+  inputField.value = "";
+});
+
 async function getWeatherData(url) {
   try {
-    const weatherData = await fetch(url);
-    if (!weatherData.ok) {
-      throw new Error(`Status: ${weatherData.status}`);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Status: ${response.status}`);
     }
-    const weatherJson = await weatherData.json();
-    console.log("Location: ", weatherJson.address);
-    console.log("Temperature:", weatherJson.days[0].temp);
-    console.log("Min Temperature: ", weatherJson.days[0].tempmin);
-    console.log("Max Temperature:", weatherJson.days[0].tempmax);
+    return response;
   } catch (error) {
     console.log(error);
   }
 }
-getWeatherData(
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lisbon/?key=BTQL28EQ6TJJ8CBVW3QNHP987",
-);
+
+async function parseJson(weatherData) {
+  try {
+    const response = await weatherData.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayWeather(parsedJson) {
+  console.log("Location: ", parsedJson.address);
+  console.log("Temperature:", parsedJson.days[0].temp);
+  console.log("Min Temperature: ", parsedJson.days[0].tempmin);
+  console.log("Max Temperature:", parsedJson.days[0].tempmax);
+}
+
+async function weatherApp(url) {
+  const weatherData = await getWeatherData(url);
+  const parsedJson = await parseJson(weatherData);
+  displayWeather(parsedJson);
+}
