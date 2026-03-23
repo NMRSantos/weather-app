5d18ac4f15c0c512fba59e87fb0bbff525d7eacb
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-03-23 00:18:39 +0000

    Add simple client temperature display

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const form = document.querySelector("form");
 const inputField = document.querySelector("input");
+const locationField = document.getElementById("location");
+const tempField = document.getElementById("current");
+const minTempField = document.getElementById("min");
+const maxTempField = document.getElementById("max");
 
 form.addEventListener("submit", (event) => {
   const userInput = inputField.value;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput}/?key=BTQL28EQ6TJJ8CBVW3QNHP987`;
   event.preventDefault();
-  weatherApp(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userInput}/?key=BTQL28EQ6TJJ8CBVW3QNHP987`,
-  );
+  weatherApp(url);
   inputField.value = "";
 });
 
@@ -52,17 +55,32 @@ function getProperties(parsedJson) {
   };
 }
 
-function displayWeatherF(address, tempF, tempMinF, tempMaxF) {
+function displayWeatherConsoleF(address, tempF, tempMinF, tempMaxF) {
   console.log("Location: ", address);
   console.log("Temperature:", tempF, "f");
   console.log("Min Temperature: ", tempMinF, "f");
   console.log("Max Temperature:", tempMaxF, "f");
 }
-function displayWeatherC(address, tempC, tempMinC, tempMaxC) {
+
+function displayWeatherConsoleC(address, tempC, tempMinC, tempMaxC) {
   console.log("Location: ", address);
   console.log("Temperature:", tempC, "c");
   console.log("Min Temperature: ", tempMinC, "c");
   console.log("Max Temperature:", tempMaxC, "c");
+}
+
+function displayWeatherF(address, tempF, tempMinF, tempMaxF) {
+  locationField.textContent = `Location: ${address}`;
+  tempField.textContent = `Temperature: ${tempF}F`;
+  minTempField.textContent = `Min Temperature: ${tempMinF}F`;
+  maxTempField.textContent = `Max Temperature: ${tempMaxF}F`;
+}
+
+function displayWeatherC(address, tempC, tempMinC, tempMaxC) {
+  locationField.textContent = `Location: ${address}`;
+  tempField.textContent = `Temperature: ${tempC}C`;
+  minTempField.textContent = `Min Temperature: ${tempMinC}C`;
+  maxTempField.textContent = `Max Temperature: ${tempMaxC}C`;
 }
 
 async function weatherApp(url) {
@@ -70,7 +88,6 @@ async function weatherApp(url) {
   const parsedJson = await parseJson(weatherData);
   const data = getProperties(parsedJson);
   displayWeatherF(data.address, data.tempF, data.tempMinF, data.tempMaxF);
-  displayWeatherC(data.address, data.tempC, data.tempMinC, data.tempMaxC);
 }
 
 const convertTemp = (fahrenheit) => {
